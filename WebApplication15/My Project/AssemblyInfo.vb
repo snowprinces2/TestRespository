934e6fb4df9e116e2d6f3ce8fb6e5d2df91df531
snowprinces2 <akito.tohma@path-finder.tokyo>
2018-04-16 16:31:46 +0900

    初めてのGithub

---
+++ WebApplication15/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は、以下の属性セットによって 
+' 制御されます。アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更します。
+
+' アセンブリの属性値を確認します
+<Assembly: AssemblyTitle("WebApplication15")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplication15")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("404165ef-8a1e-4ce8-9efd-fd111c036c79")>
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
